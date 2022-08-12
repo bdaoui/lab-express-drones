e188d776df7a4712f5c5c65cb99832b97ecb31fc
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-08-12 14:02:39 +0200

    not that done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,22 @@
 // Iteration #1
+
+const mongoose = require("mongoose"); // require mongoose 
+const {Schema } = mongoose; // deconstructure both schema and module
+
+
+// set new Schema
+
+const droneSchema = new Schema({
+    name: {type: String},
+    propellers: {type: String},
+    maxSpeed: {type: Number}
+});
+
+
+// set new model
+
+const Drone = mongoose.model("Drone", droneSchema);
+
+// export the newly minted model
+
+module.exports = Drone;
--- routes/drones.js
+++ routes/drones.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+
 // require the Drone model here
+const Drone = require("../models/Drone.model.js")
+
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+    .then( result => {
+      // console.log(result);
+      res.render("drones/list", result) }
+      )
+    .catch(error => "this is an error") 
 });
 
 router.get('/drones/create', (req, res, next) => {
