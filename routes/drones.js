3a81daac4db6f86c099c0c901e9c454d0016e44d
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-08-13 01:57:42 +0200

    done all iterations

--- routes/drones.js
+++ routes/drones.js
@@ -57,6 +57,7 @@ router.post('/drones/:id/edit', (req, res, next) => {
 
 
   // I worked so hard for this code, I cannot even believe I managed to find the solution. This is 2 am, I started working on it just after course.
+
   Drone.updateOne({_id:id}, {$set: {name: newDrone.name, propellers: newDrone.propellers, maxSpeed: newDrone.maxSpeed }})
     .then(result =>{
       res.render("drones/update-form", {result})
@@ -64,7 +65,7 @@ router.post('/drones/:id/edit', (req, res, next) => {
     .then( result =>{
       res.redirect("/drones");
     })
-    .catch(error =>  "this is an error")
+    .catch(error =>  "this is an error in the updating operation")
 
 });
 
@@ -73,6 +74,12 @@ router.post('/drones/:id/edit', (req, res, next) => {
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
+  const {id} = req.params;
+  Drone.findByIdAndDelete(id)
+  .then( result =>{
+    res.redirect("/drones");
+  })
+  .catch(error =>  "this is an error in the deletion operation")
 });
 
 module.exports = router;
